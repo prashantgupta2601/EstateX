9c6addf29ac4270fc1ab5d248604086f96f65078
Prashant Gupta <prashantguptagzp2708@gmail.com>
2026-07-07 15:42:14 +0530

    fix: add placehold.co to Next.js remotePatterns whitelisted domains

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'wallpaperaccess.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
     ],
   },
 };
